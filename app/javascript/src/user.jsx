694e045634f936e8bffa88e9b70ad2815bb9be96
Zach <zkdoran@gmail.com>
2023-07-19 18:18:03 -0400

    current stopping point. cant get map to work

--- app/javascript/src/user.jsx
+++ app/javascript/src/user.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import Layout from './layout';
-import { getTweets, getOneTweet, getUserTweets, deleteTweet, searchTweets, logoutUser } from './utils';
+import { getTweets, getOneTweet, getUserTweets, deleteTweet, searchTweets, logoutUser, authenticateUser } from './utils';
 
 const User = props => {
   const location = useLocation();
@@ -9,14 +9,15 @@ const User = props => {
   const [tweets, setTweets] = useState([]);
   const [search, setSearch] = useState('');
 
-  const getTweets = () => {
-    getUserTweets(username)
+  const getAllTweets = () => {
+    getTweets(username)
       .then(data => {
         setTweets(data);
       })
   }
 
   const deleteOneTweet = (id) => {
+    authenticateUser()
     deleteTweet(id)
       .then(data => {
         getTweets();
@@ -46,66 +47,92 @@ const User = props => {
       })
   }
 
+  const getUserTweets = (username) => {
+    getUserTweets(username)
+      .then(data => {
+        setTweets(data);
+      })
+  }
+
   const handleChange = (e) => {
     setSearch(e.target.value);
   }
 
+  useEffect(() => {
+    getAllTweets();
+  }, [])
 
   return (
-      <div className="main container mt-5">
-        <div className="row">          
-          <div className="user-field col">
-            <a className="username" href="#">@{username}</a><br/>
-            <div className="col-xs-3">
-              <a href="">
-                <span>Tweets<br/></span>
-                <span className="user-stats-tweets">10</span>
-              </a>
-            </div>
-            <div className="col-xs-4">
-              <a href="">
-                <span>Following<br/></span>
-                <span className="user-stats-following">0</span>
-              </a>
-            </div>
-            <div className="col-xs-4">
-              <a href="">
-                <span>Followers<br/></span>
-                <span className="user-stats-followers">0</span>
-              </a>
-            </div>
+    <div className="main container mt-5">
+      <div className="row">          
+        <div className="user-field col">
+          <a className="username" href="#">@{username}</a><br/>
+          <div className="col-xs-3">
+            <a href="">
+              <span>Tweets<br/></span>
+              <span className="user-stats-tweets">10</span>
+            </a>
           </div>
-          <div className="col feed-box">
-            <div className="col-xs-12 post-tweet-box">
-              <textarea type="text" className="form-control post-input" rows="3" placeholder="What's happening?"></textarea>
-              <div className="pull-right">
-                <button className="btn btn-primary" disabled id="post-tweet-btn">Tweet</button>
-              </div>
-            </div>
-            <div className="feed">             
-            </div>
+          <div className="col-xs-4">
+            <a href="">
+              <span>Following<br/></span>
+              <span className="user-stats-following">0</span>
+            </a>
           </div>
-          <div className="trends col">
-            <div className="col-xs-12">
-              <form onSubmit={searchTweets}>
-                <input type="text" className="form-control" placeholder="Search Twitter" onChange={handleChange} />
-              </form>
-              <div className="trends-header">
-                <span>Trends</span><span> &#183; </span><small><a href="">Change</a></small>
-              </div>
-              <ul className="trends-list">
-                <li><a href="#">#React</a></li>
-                <li><a href="#">#Ruby</a></li>
-                <li><a href="#">#foobarbaz</a></li>
-                <li><a href="#">#rails</a></li>
-                <li><a href="#">#API</a></li>
-              </ul>
-            </div>
-          </div>          
-          <div className="col follow-suggest">
+          <div className="col-xs-4">
+            <a href="">
+              <span>Followers<br/></span>
+              <span className="user-stats-followers">0</span>
+            </a>
+          </div>
+          <div className="col-xs-12">
+            <button className="btn btn-primary" onClick={logout}>Logout</button>
           </div>
         </div>
+        <div className="col feed-box">
+          <div className="col-xs-12 post-tweet-box">
+            <textarea type="text" className="form-control post-input" rows="3" placeholder="What's happening?"></textarea>
+            <div className="pull-right">
+              <button className="btn btn-primary" disabled id="post-tweet-btn">Tweet</button>
+            </div>
+          </div>
+          <div className="feed">
+            {tweets.map(tweet => {
+              return (
+                <div className="tweet" key={tweet.id}>
+                  <p>{tweet.tweet}</p>
+                  <div className="tweet-footer">
+                    <small><a href="#">{tweet.username}</a></small>
+                    <small><a href="#">{tweet.created_at}</a></small>
+                    <small><a href="#" onClick={deleteOneTweet}>Delete</a></small>
+                  </div>
+                </div>
+              )
+            })
+          }
+          </div>
+        </div>
+        <div className="trends col">
+          <div className="col-xs-12">
+            <form onSubmit={searchTweets}>
+              <input type="text" className="form-control" placeholder="Search Twitter" onChange={handleChange} />
+            </form>
+            <div className="trends-header">
+              <span>Trends</span><span> &#183; </span><small><a href="">Change</a></small>
+            </div>
+            <ul className="trends-list">
+              <li><a href="#">#React</a></li>
+              <li><a href="#">#Ruby</a></li>
+              <li><a href="#">#foobarbaz</a></li>
+              <li><a href="#">#rails</a></li>
+              <li><a href="#">#API</a></li>
+            </ul>
+          </div>
+        </div>          
+        <div className="col follow-suggest">
+        </div>
       </div>
+    </div>
   )
 }
 
