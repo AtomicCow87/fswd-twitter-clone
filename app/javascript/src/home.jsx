b8798e7015a66fb8a5b82c5ddb57b872534d4836
Zach <zkdoran@gmail.com>
2023-07-25 11:55:31 -0400

    working main page

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { handleErrors } from './utils/fetchHelper';
+import { handleErrors, safeCredentials } from './utils/fetchHelper';
 
 import './home.scss';
 import Layout from './layout';
@@ -10,9 +10,22 @@ class Home extends React.Component {
     tweets: [],
     loading: true,
     authenticated: false,
+    error: '',
   }
 
   componentDidMount() {
+    fetch('/api/authenticated')
+      .then(handleErrors)
+      .then(data => {
+        this.setState({
+          authenticated: data.authenticated,
+        })
+      })
+
+    this.getTweets();
+  }
+
+  getTweets = () => {
     fetch('/api/tweets')
       .then(handleErrors)
       .then(data => {
@@ -21,36 +34,50 @@ class Home extends React.Component {
           loading: false,
         })
       })
+  }
 
-    fetch('/api/authenticated')
+  logoutUser = (e) => {
+    if (e) { e.preventDefault(); }
+    this.setState({
+      error: '',
+    });
+
+    fetch('/api/session', safeCredentials({
+      method: 'DELETE',
+    }))
       .then(handleErrors)
       .then(data => {
+        if (data.success) {
+          this.setState({
+            authenticated: false,
+          });
+        }
+      })
+      .catch(error => {
         this.setState({
-          authenticated: data.authenticated,
+          error: 'Could not log out.',
         })
       })
   }
 
-  render() {
-    const { tweets, loading } = this.state;
 
-    if (loading) {
-      return (
-        <Layout>
-          <h4>Loading...</h4>
-        </Layout>
-      )
-    }
+  render() {
+    const { tweets, loading, authenticated } = this.state;
 
     return (
       <Layout>
         <div className="row">
           <div className="col">
-            <div className="mb-3">
-              <h1>left side</h1>
+            <div className="d-grid col-6 mx-auto">
+              {authenticated && <a href="/" className="btn btn-primary" onClick={logoutUser}>Log out</a>}
+              {!authenticated && <a href="/login" className="btn btn-primary rounded-pill mb-3">Log in or Sign up</a>}
             </div>
           </div>
-          <div className="home col">
+          <div className="home col mx-5">
+            {loading && <div className="spinner-border text-primary" role="status">loading...</div>}
+            {(loading === false && authenticated === false) && 
+              <div className="alert alert-info" role="alert">Please login to post a tweet</div>
+            }
             <div className="tweets">
               <p>middle</p>
               {tweets.map(tweet => (
@@ -68,7 +95,7 @@ class Home extends React.Component {
               ))}
             </div>
           </div>
-          <div className="col">
+          <div className="col mx-5">
             <div className="mb-3">
               <h1>right side</h1>
             </div>
