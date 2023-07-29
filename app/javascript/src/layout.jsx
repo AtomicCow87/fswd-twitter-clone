a2e97c5d3aca136a0c0b759d1434308f40fecee1
Zach <zkdoran@gmail.com>
2023-07-28 20:51:57 -0400

    added icons to layout

--- app/javascript/src/layout.jsx
+++ app/javascript/src/layout.jsx
@@ -1,12 +1,17 @@
 // layout.js
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faSquareTwitter } from '@fortawesome/free-brands-svg-icons';
+
 
 const Layout = (props) => {
   return (
     <React.Fragment>
       <nav className="navbar navbar-expand navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand text-info" href="/">Twitter</a>
+          <a className="navbar-brand text-info" href="/">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto">
               <li className="nav-item">
@@ -19,7 +24,12 @@ const Layout = (props) => {
       {props.children}
       <footer className="p-3 bg-light">
         <div>
-          <p className="me-3 mb-0 text-secondary">Twitter Clone</p>
+          <p className="me-3 mb-0 text-secondary">
+            <a href="https://fontawesome.com/license/free" target="_blank" rel="noopener noreferrer" className="ml-2">
+              <FontAwesomeIcon icon={faSquareTwitter} className="mx-2" />
+            </a>
+            Twitter Clone
+          </p>
         </div>
       </footer>
     </React.Fragment>
